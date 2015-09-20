024c6c198847b6828dc99cc2aa62f86b945ed334
Markus Malkusch <markus@malkusch.de>
2015-09-20 13:51:05 +0200

    Workaround for padraic/mockery#268

--- tests/PHPMockeryTest.php
+++ tests/PHPMockeryTest.php
@@ -4,6 +4,7 @@ namespace phpmock\mockery;
 
 use phpmock\AbstractMockTest;
 use Mockery;
+use Mockery\MockInterface;
 
 /**
  * Tests PHPMockery.
@@ -24,5 +25,38 @@ class PHPMockeryTest extends AbstractMockTest
     protected function mockFunction($namespace, $functionName, callable $function)
     {
         PHPMockery::mock($namespace, $functionName)->andReturnUsing($function);
+    }
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $this->workaroundMockeryIssue268();
+    }
+    
+    /**
+     * Workaround for Mockery's issue 268.
+     *
+     * Mockery-0.9 introduced global memoization of reflection methods. This
+     * workaround clears that memoization to fix the affected tests.
+     *
+     * @link https://github.com/padraic/mockery/issues/268 Issue 268
+     */
+    private function workaroundMockeryIssue268()
+    {
+        foreach (get_declared_classes() as $class) {
+            if (!is_subclass_of($class, MockInterface::class)) {
+                continue;
+                
+            }
+            try {
+                $_mockery_methods = new \ReflectionProperty($class, "_mockery_methods");
+                $_mockery_methods->setAccessible(true);
+                $_mockery_methods->setValue(null);
+
+            } catch (\ReflectionException $e) {
+                // The unaffected version mockery-0.8 didn't had that property.
+            }
+        }
     }
 }
